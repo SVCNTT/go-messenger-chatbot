e597291576031a349f326af94379367998641963
Toan MK <toanmk@scommerce.asia>
2018-11-27 14:06:52 +0700

    Update TSL

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"github.com/labstack/gommon/log"
 )
 
 func main()  {
 	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello")
 	})
-	http.ListenAndServe(":8091", nil)
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 
